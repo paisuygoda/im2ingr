b84ff9484588db2c6483b3066e6d7f7e04c2f45a
paisuygoda <paisuygoda3@gmail.com>
2018-01-18 11:16:08 +0900

    add multilabel_test

--- multilabel_test.py
+++ multilabel_test.py
@@ -151,7 +151,7 @@ def test(test_loader, model, criterion):
 
         target_var = list()
         for j in range(len(target)-2):
-            target[j] = torch.autograd.Variable(torch.Tensor(float(target[j]))).cuda(async=True)
+            target[j] = torch.autograd.Variable(target[j]).cuda(async=True)
             target_var.append(torch.autograd.Variable(target[j]))
 
         # compute loss
